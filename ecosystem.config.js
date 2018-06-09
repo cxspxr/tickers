f5b5a2df61f215c0169d1d1d3996ac9d515b0866
casper <ycasperinho@gmail.com>
2018-06-10 00:46:01 +0300

    add usd to rub module

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,11 @@ module.exports = {
       name      : 'tickers',
       script    : 'tickers/tickers.js',
       watch     : ['tickers/watch-me.js']
+    },
+    {
+      name      : 'rate',
+      script    : 'rate/rate.js',
+      watch     : false
     }
   ]
 };
---
+++ rate/rate.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+const mysql = require('../database/db');
+
+const interval = 1000 * 60 * 30; // half of an hour
+
+setInterval(() => {
+    axios.get('https://free.currencyconverterapi.com/api/v5/convert?q=USD_RUB&compact=ultra')
+        .then( (response) => {
+            mysql.connect();
+            var query = 'update configs set currency_rate = ' + response.data.USD_RUB + ' limit 1';
+            mysql.query(query, function (error) {
+                if (error) throw err;
+            });
+            mysql.end();
+        });
+}, interval);
